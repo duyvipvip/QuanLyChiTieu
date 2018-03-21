45f6703000cc71315805fd1f0ece337cea0eda50
havanduy <havanduy709@gmail.com>
2018-03-21 13:03:07 +0700

    test3

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+console.log(__dirname + '/dist');
 
 // Start the app by listening on the default Heroku port
 const PORT = process.env.PORT || 8080;
